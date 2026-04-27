1cbcbc5dec9796a0cfb0b79f853bd5a17f7b4ee2
Nga Nguyen <kyo1508@gmail.com>
2026-04-27 17:23:26 +0700

    add nvim tree keymap

--- nvim/.config/nvim/lua/core/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/core/plugins/nvim-tree.lua
@@ -3,14 +3,12 @@
 return {
   {
     'nvim-tree/nvim-tree.lua',
-    cmd = { 'NvimTreeToggle', 'NvimTreeFindFile' }, -- lazy load on command
+    cmd = { 'NvimTreeToggle', 'NvimTreeFindFile', 'NvimTreeRefresh', 'NvimTreeCollapse' },
     keys = {
-      {
-        -- open at current file: <cmd>NvimTreeFindFileToggle<cr>
-        '<leader>e',
-        '<cmd>NvimTreeToggle<cr>',
-        desc = 'Togglle file tree',
-      },
+      { '<leader>ee', '<cmd>NvimTreeToggle<CR>',   desc = 'Explorer [E]xplorer toggle' },
+      { '<leader>ef', '<cmd>NvimTreeFindFile<CR>', desc = 'Explorer [F]ind file' },
+      { '<leader>er', '<cmd>NvimTreeRefresh<CR>',  desc = 'Explorer [R]efresh' },
+      { '<leader>ec', '<cmd>NvimTreeCollapse<CR>', desc = 'Explorer [C]ollapse' },
     },
     dependencies = {
       'nvim-tree/nvim-web-devicons',
