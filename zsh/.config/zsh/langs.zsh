260bc83b5b3562fe4e34ef541369eadb5a31f606
Nga Nguyen <kyo1508@gmail.com>
2026-06-03 15:47:53 +0700

    zsh: use version file strategy recursive for fnm command

--- zsh/.config/zsh/langs.zsh
+++ zsh/.config/zsh/langs.zsh
@@ -2,5 +2,5 @@
 
 # fnm - fast Node version manager (Homebrew). Reads .nvmrc / .node-version.
 if (( $+commands[fnm] )); then
-  eval "$(fnm env --use-on-cd --shell zsh)"
+  eval "$(fnm env --use-on-cd --version-file-strategy=recursive --shell zsh)"
 fi
